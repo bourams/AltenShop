3c9e24d5f383faa1e8a5e99579ab29b753ded34c
bourams <brahim.benmansour@outlook.fr>
2025-06-17 21:07:24 +0200

    added db context with entity framework and SLQ Server packages

--- AltenApi/Program.cs
+++ AltenApi/Program.cs
@@ -1,7 +1,14 @@
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
