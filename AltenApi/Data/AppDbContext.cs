8fdeac0a054cf46a4c4701afbc8e7ea8d454d34f
bourams <brahim.benmansour@outlook.fr>
2025-06-18 11:29:13 +0200

    Added product model and migration

--- AltenApi/Data/AppDbContext.cs
+++ AltenApi/Data/AppDbContext.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Api.Data.Models;
+using Api.Data.ModelsConfig;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<Product> Products { get; set; }
+
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
-    // public DbSet<Product> Products { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        base.OnModelCreating(modelBuilder);
+    }
 }
---
+++ AltenApi/Data/ModelsConfig/ProductConfiguration.cs
@@ -0,0 +1,62 @@
+﻿using Api.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Data.ModelsConfig
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+
+            // Primary key
+            builder.HasKey(p => p.Id);
+
+            // Properties
+            builder.Property(p => p.Code)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(1000);
+
+            builder.Property(p => p.Image)
+                .HasMaxLength(500);
+
+            builder.Property(p => p.Category)
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Price)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(p => p.Quantity)
+                .IsRequired();
+
+            builder.Property(p => p.InternalReference)
+                .HasMaxLength(100);
+
+            builder.Property(p => p.ShellId)
+                .IsRequired();
+
+            builder.Property(p => p.InventoryStatus)
+                   .HasConversion<string>()
+                   .IsRequired()
+                   .HasMaxLength(20);
+
+            builder.Property(p => p.Rating)
+                .IsRequired();
+
+            builder.Property(p => p.CreatedAt)
+                .IsRequired();
+
+            builder.Property(p => p.UpdatedAt)
+                .IsRequired();
+        }
+    }
+}
